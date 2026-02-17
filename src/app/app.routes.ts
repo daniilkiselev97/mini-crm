01883e3a9f75b7ad18dc9991e0144e3fb6249a63
Daniil Kiselev <danqkiselev@yandex.ru>
2026-02-17 15:43:22 +0300

    feat: setup layout and routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: Layout,
+		children: [
+			{
+				path: 'dashboard',
+				loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard)
+			},
+			{
+				path: 'customers',
+				loadComponent: () => import('./features/customers/customers').then(m => m.Customers)
+			},
+			{
+				path: '',
+				redirectTo: 'dashboard',
+				pathMatch: 'full'
+			}
+		]
+
+	},
+
+];
